41a06559974eee918f2c48b88a51941ff7f4891f
mattsi <mattsi@jansky.co.uk>
2018-07-19 20:59:46 +0100

    Adds random entity placer
    Needed a simple implementation inside non-test project

---
+++ src/Crawler/Crawler/Maps/EntityPlacers/RandomEntityPlacer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Crawler.Queryables.Entities;
+
+namespace Crawler.Maps.EntityPlacers
+{
+    public class RandomEntityPlacer :IEntityPlacer
+    {
+        Random random = new Random();
+
+        public Point PlaceCharacter(IMap map, Entity entity)
+        {
+            var position = new Point(random.Next(map.Size.X - 1), random.Next(map.Size.Y - 1));
+            map.Add(entity, position);
+            return position;
+        }
+    }
+}
