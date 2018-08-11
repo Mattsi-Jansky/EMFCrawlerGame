bf8438279c29c6abb631309ee0fd471c2c0ce0f4
mattsi <mattsi@jansky.co.uk>
2018-08-11 22:43:37 +0100

    Improves SimpleDungeonGenerator
    These variables generate more interesting dungeons at this size

--- src/Crawler/Crawler/Maps/Initialisers/DungeonGenerators/SimpleDungeonGenerator.cs
+++ src/Crawler/Crawler/Maps/Initialisers/DungeonGenerators/SimpleDungeonGenerator.cs
@@ -11,7 +11,7 @@ namespace Crawler.Maps.Initialisers.DungeonGenerators
 
         public override IMap Initialise()
         {
-            DungeonGenerator generator = new DungeonGenerator(60,30, 3, 8);
+            DungeonGenerator generator = new DungeonGenerator(60,30, 2, 4);
             var translator = new DungeonGenerationModelTranslator(generator);
             return translator.Initialise();
         }
