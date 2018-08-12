eaa8aa2c9c2e1de23cacd4c9d08a6f8e69fc1ff3
mattsi <mattsi@jansky.co.uk>
2018-08-12 14:30:01 +0100

    Adds mobs to graphics-generator.py
    Also updates graphics-enums.txt

--- src/Crawler/graphics-generator.py
+++ src/Crawler/graphics-generator.py
@@ -3,7 +3,24 @@ races = ["Barbarian", "Cyclops", "DarkElf", "Devilspawn", "Drawconian", "Dwarf",
 
 envTiles = ["NWall", "NWallTorch", "NWallTorch2", "DamagedNWall", "Wall", "Floor", "SpiralStaircase", "StairsUp", "StairsDown", "Hole", "Hatch", "HatchOpen", "AltFloor", "FancyFloor", "Nothing", "Nothing2"]
 envColours = ["Gray", "Brown", "Blue", "Red"]
-envTilesStart = 402
+envTilesStart = 403
+
+mobs = ["RangerMale", "WarriorMale", "BlueMageMale", "DwarfMale", "RedMageMale", "YellowPirateMale", "MonkMale", "RogueMale", "KnightMale", "RedKnightMale", "YellowKnightMale", "PirateMale", "VikingMale", "AbbotMale", "RobinHoodMale", "BarbarianMale",
+"RangerFemale", "WarriorFemale", "BlueMageFemale", "DwarfFemale", "RedMageFemale", "YellowPirateFemale", "MonkFemale", "RogueFemale", "KnightFemale", "RedKnightFemale", "YellowKnightFemale", "PirateFemale", "VikingFemale", "AbbotFemale", "RobinHoodFemale", "BarbarianFemale",
+"Moses", "Villiager", "Chef", "Thief", "Soldier", "BlueMonk", "WhiteMonk", "RedMonk", "RedPope", "Nun", "SpookyCapeDark", "SpookyCapeWhite", "SpookyCapeRed", "CreeperBlack", "CreeperPurple", "Witch",
+"ElfRangerAlt", "ElfWarriorAlt", "ElfWizard", "ElfArcherAlt", "ElfPrincess", "IceArcher", "IceWarrior", "IceWizard", "IceArcherAlt", "IcePrincess", "DarklingArcher", "DarklingWarrior", "DarklingWizard", "DarklingArcherAlt", "DarklingPrincess", "DarklingRogue",
+"OldWarrior", "OldAxeman", "OldWizard", "OldMacer", "OldKing", "IceGiantWarrior", "IceGiantAxeman", "IceGiantWizard", "IceGiantMacer", "IceGiantKing", "SpookyWeirdMacer", "HalflingRogue", "HalflingArcher", "HalflingWizar", "DarkHalflingRogue", "DarkHalflingArcher",
+"Imp", "ImpWarriorAlt", "ImpArmoredWarrior", "ImpMageAlt", "Troll", "TrollWarrior", "RedTroll", "RedTrollWarrior", "BlueSomething", "WhiteSomething", "GraySomething", "ChissArcher", "ChissSwordsman", "ChissMage", "ChissKing", "ChissQueen",
+"Ghost", "GhostWarrior", "GhostArmouredWarrior", "GhostQueen", "GhostKing", "FlameSkull", "IceSkull", "IceSpectre", "ShadowSpectre", "ShadowSpectreAlt", "VHS", "VHSKing", "VHSQueen", "Death", "Vampire", "VampireKing",
+"ElementalFire", "ElementalWater", "ElementalIce", "DuckBlue", "DuckRed", "DuckGray", "PhantomGray", "PhantomBlue", "PhantomWhite", "PhantomBat", "UndeadGhostArcher", "UndeadGhostWarrior", "UndeadGhostMage", "UndeadGhostRogue", "UndeadGhostBig", "UndeadGostKnight", 
+"GiantBarbarianMale", "GiantBarbarianFemale", "GiantWarrior", "GiantMonk", "GiantKing", "OgreWarrior", "OgreWarriorGray", "OgreWarriorArmoured", "OgreWarriorArmouredGray", "OgreKing", "GoblinWarrior", "GoblingArmouredWarrior", "GoblinArmouredShieldedWarrior", "GoblinMage", "GoblinQueen", "GoblinKing",
+"Demon", "HiteDemon", "DemonImp", "DemonImpWarrior", "DemonImpPrincess", "DemonImpFatBlue", "DemonImpFatYellow", "ValkyrieMale", "ValkyrieFemale", "ChissBadHair", "FatKnight", "BlueSpark", "BlueSparkBigger", "DecaptitatedHeadMale", "DecapitatedHeadFemale", "DecapitedHeadGhost",
+"RockColossus", "StoneCyclopsColossus", "EarthColossus", "GrassColossus", "EarthCyclopsColossus", "IceColossus", "GreenColossus", "PurpleColossus", "GhostColossus", "PaleColossus", "FireColossus", "BlackColossus", "WaterColossus", "SmokeColossus", "YellowCyclopsColossus", "RedCyclopsColossus",
+"SlimeBlack", "SlimeBrown", "SlimeGreen", "EyeSmall", "EyeBig", "EyeReallyBig", "Ent", "EntWithLeaves", "Ship", "ShipPink", "Yeti", "YetiGray", "BoxRed", "BoxPurple", "BoxGreen", "Mimic",
+"CatBrown", "CatGray", "CatBlack", "CatTabby", "SnakePurple", "SnakeGreen", "ScorpionPale", "ScorpionBlack", "Spider", "SpiderRed", "SpiderGreen", "WhatTheHellIsThat", "Fish", "CrapFish", "Bat", "BatDark",
+"PupperDark", "PupperBrown", "Sheep", "Bull", "Bird", "Oxen", "Gorilla", "Monkey", "SkinnyMonkey", "Horse", "Deer", "Ewok", "OldHorse", "GrayHorse", "WhiteHorse", "Lion",
+"DemonDogRed", "DemonDogYellow", "DemonDogGreen", "DemonDogBlue", "DemonDogGray", "EggWhite", "EggWhiteOpen", "EggYellow", "EggYellowOpen", "EggGreen", "EggGreenOpen", "EggBlue", "EggBlueOpen", "FloatySnakeRed", "FloatySnakeBrown", "FloatySnakeYellow"]
+mobsStart = 467
 
 print "found %s races" % (len(races))
 print "found %s classes" % (len(classes))
@@ -40,7 +57,12 @@ print ""
 print "EnvTiles+Colours numbers"
 for c in range(len(envColours)):
     for t in range(len(envTiles)):
-        print(envTilesStart + (c * len(envTiles)) + c, envTiles[t] + " " + envColours[c])
+        print(envTilesStart + (c * len(envTiles)) + t, envTiles[t] + " " + envColours[c])
+
+print ""
+print "Mobs numbers "
+for m in range(len(mobs)):
+    print("(%s, '%s')" % (mobsStart + m, mobs[m]))
 
 print ""
 print "Enum entries"
@@ -50,3 +72,5 @@ for r in range(len(races)):
 for c in range(len(envColours)):
     for t in range(len(envTiles)):
         print("        %s%s," % (envTiles[t], envColours[c]))
+for m in range(len(mobs)):
+    print("        %s," % (mobs[m]))
