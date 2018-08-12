956bfb111de723f541c36043ca32e69733172986
mattsi <mattsi@jansky.co.uk>
2018-08-12 13:25:21 +0100

    Updates graphics-generator to use env tiles
    Did this a while back but managed to not commit it somehow

--- src/Crawler/graphics-generator.py
+++ src/Crawler/graphics-generator.py
@@ -1,8 +1,15 @@
 classes = ["Warlock", "HellKnight", "Druid", "HighMage", "Mage", "Mindcrafter", "Mystic", "Paladin", "Priest", "Ranger", "Rogue", "Warrior", "WarriorMage"]
-races = ["Barbarian", "Cyclops", "DarkElf", "Devilspawn", "Drawconian", "Dwarf", "Elf", "Gnome", "Golem", "HalfElf", "HalfGiant", "HalfOgre", "HalfOrc","HalfTitan", "HalfTroll", "HighElf", "Hobbit", "Human", "Imp", "Klackon", "Kobold", "MindFlayer", "Nephilim", "Nibelung", "Saracen", "Skeleton", "Sprite", "Vampire", "Yeek", "Zombie"]
+races = ["Barbarian", "Cyclops", "DarkElf", "Devilspawn", "Drawconian", "Dwarf", "Elf", "Gnome", "Golem", "HalfElf", "HalfGiant", "HalfOgre", "HalfOrc","HalfTitan", "HalfTroll", "HighElf", "Hobbit", "Human", "Imp", "Klackon", "Kobold", "MindFlayer", "Nephilim", "Nibelung", "Saracen", "Skeleton", "Spectre", "Sprite", "Vampire", "Yeek", "Zombie"]
+
+envTiles = ["NWall", "NWallTorch", "NWallTorch2", "DamagedNWall", "Wall", "Floor", "SpiralStaircase", "StairsUp", "StairsDown", "Hole", "Hatch", "HatchOpen", "AltFloor", "FancyFloor", "Nothing", "Nothing2"]
+envColours = ["Gray", "Brown", "Blue", "Red"]
+envTilesStart = 402
 
 print "found %s races" % (len(races))
 print "found %s classes" % (len(classes))
+
+print "found %s envTiles" % (len(envTiles))
+print "found %s envColours" % (len(envColours))
 
 print "Classes"
 for i in range(len(classes)):
@@ -14,13 +21,32 @@ for i in range(len(races)):
     print(i, races[i])
 
 print ""
+print "envTiles"
+for i in range(len(envTiles)):
+    print(i, envTiles[i])
+
+print ""
+print "envColours"
+for i in range(len(envColours)):
+    print(i, envColours[i])
+
+print ""
 print "Race+Class numbers"
 for r in range(len(races)):
     for c in range(len(classes)):
         print((r * len(classes)) + c, races[r] + " " + classes[c])
-    
+
+print ""
+print "EnvTiles+Colours numbers"
+for c in range(len(envColours)):
+    for t in range(len(envTiles)):
+        print(envTilesStart + (c * len(envTiles)) + c, envTiles[t] + " " + envColours[c])
+
 print ""
 print "Enum entries"
 for r in range(len(races)):
     for c in range(len(classes)):
         print("        %s%s," % (races[r], classes[c]))
+for c in range(len(envColours)):
+    for t in range(len(envTiles)):
+        print("        %s%s," % (envTiles[t], envColours[c]))
