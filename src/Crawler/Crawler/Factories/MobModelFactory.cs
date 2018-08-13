2a726786288b211376e4d667010a36b02063a4c3
mattsi <mattsi@jansky.co.uk>
2018-08-13 21:03:30 +0100

    Adds more mob types

--- src/Crawler/Crawler/Factories/MobModelFactory.cs
+++ src/Crawler/Crawler/Factories/MobModelFactory.cs
@@ -24,12 +24,27 @@ namespace Crawler.Factories
         private static readonly MobModel GoblingArmouredWarrior = new MobModel("Goblin", Graphic.GoblingArmouredWarrior);
         private static readonly MobModel GoblinArmouredShieldedWarrior = new MobModel("Goblin", Graphic.GoblinArmouredShieldedWarrior);
         private static readonly MobModel GoblinMage = new MobModel("Goblin Magicker", Graphic.GoblinMage);
+        private static readonly MobModel Troll = new MobModel("Troll", Graphic.Troll);
+        private static readonly MobModel TrollWarrior = new MobModel("Troll", Graphic.TrollWarrior);
+        private static readonly MobModel OgreWarrior = new MobModel("Ogre", Graphic.OgreWarrior);
+        private static readonly MobModel OgreWarriorGray = new MobModel("Ogre", Graphic.OgreWarriorGray);
+        private static readonly MobModel OgreWarriorArmoured = new MobModel("Ogre", Graphic.OgreWarriorArmoured);
+        private static readonly MobModel OgreWarriorArmouredGray = new MobModel("Ogre", Graphic.OgreWarriorArmouredGray);
+        private static readonly MobModel GiantWarrior = new MobModel("Giant", Graphic.GiantWarrior);
+        private static readonly MobModel GiantBarbarianMale = new MobModel("Giant", Graphic.GiantBarbarianMale);
+        private static readonly MobModel GiantBarbarianFemale = new MobModel("Giant", Graphic.GiantBarbarianFemale);
+        private static readonly MobModel GiantMonk = new MobModel("Giant", Graphic.GiantMonk);
+        
+        private static readonly MobModel RedTroll = new MobModel("Troll", Graphic.RedTroll);
         
         public static readonly IList<IList<MobModel>> MobThemes = new List<IList<MobModel>>
         {
             new List<MobModel> { SkeletonArcher, SkeletonWarrior, SkeletonRogue, SkeletonKnight, SkeletonWarlock},
             new List<MobModel> {Spider, SpiderRed, SpiderGreen, CatBlack, SnakePurple, SnakeGreen, ScorpionPale, ScorpionBlack, Bat, BatDark},
             new List<MobModel> {GoblinWarrior, GoblinArmouredShieldedWarrior, GoblingArmouredWarrior, GoblinMage},
+            new List<MobModel> {Troll, TrollWarrior, RedTroll},
+            new List<MobModel> {OgreWarrior, OgreWarriorGray, OgreWarriorArmoured, OgreWarriorArmouredGray},
+            new List<MobModel> {GiantWarrior, GiantBarbarianFemale, GiantBarbarianMale, GiantMonk},
             
         };
     }
